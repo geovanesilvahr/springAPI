0b63107c66cb93d509108d75d2e2cd02babdde52
Geovane Silva <geovanesilva.hr@gmail.com>
2021-08-18 18:01:00 -0300

    Increntando a conexão com DB Postgre - v0.0.2

--- src/main/java/com/springAPI/model/Especialidade.java
+++ src/main/java/com/springAPI/model/Especialidade.java
@@ -1,5 +1,55 @@
 package com.springAPI.model;
 
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.sun.istack.NotNull;
+
+@Entity
+@Table(name="tb_especialidade")
 public class Especialidade {
+	
+	@Column(name = "esp_codigo")
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo;
+	@Column(name = "esp_nome")
+	@NotNull
+	private Long nome;
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(codigo);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Especialidade other = (Especialidade) obj;
+		return Objects.equals(codigo, other.codigo);
+	}
+	
+	public Long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+	public Long getNome() {
+		return nome;
+	}
+	public void setNome(Long nome) {
+		this.nome = nome;
+	}
 
 }
