5a836a8321c6d10fdf351a1b3cc3fa28e629978d
Geovane Silva <geovanesilva.hr@gmail.com>
2021-09-29 17:13:51 -0300

    Implementando o POST da classe Esp. - v0.0.8

--- src/main/java/com/springAPI/resource/EspecialidadeResource.java
+++ src/main/java/com/springAPI/resource/EspecialidadeResource.java
@@ -3,8 +3,12 @@ package com.springAPI.resource;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springAPI.model.Especialidade;
@@ -20,6 +24,12 @@ public class EspecialidadeResource {
 	@GetMapping
 	private List<Especialidade> listarTodos(){
 		return especialidadeRepository.findAll();
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	private void criar(@RequestBody Especialidade especialidade) {
+		especialidadeRepository.save(especialidade);
 	}
 	
 	
